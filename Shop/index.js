a0407a5504699fdafc772aa0f97afd4cd184390a
John Doe <johndoe@example.com>
2022-11-11 20:17:10 +0700

    Vue.js

--- Shop/index.js
+++ Shop/index.js
@@ -1,11 +1,77 @@
+const items = [
+    {
+        img: `./res/catalog (1).png`,
+        name: 'Бородатый мужчина',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (2).png`,
+        name: 'Смазливый мальчик',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (3).png`,
+        name: 'Блондиночка',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (4).png`,
+        name: 'Секретарша',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (5).png`,
+        name: 'Зебра',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (6).png`,
+        name: 'Модник огородник',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (7).png`,
+        name: 'Ноги',
+        price: 1000,
+    },
+    {
+        img: `./res/catalog (8).png`,
+        name: 'Священник',
+        price: 1000,
+    },
+];
+let isShowedAllItems = false;
 const app = new Vue({
     el: '#app',
     data: {
-        items: [],
+        items,
     },
     methods: {
         browseAllClickHandler() {
-            console.log('Здесь должны развернуться товары');
+            if (isShowedAllItems) {
+                // убираем лишнее
+                isShowedAllItems = false;
+            } else {
+                const catalog = document.querySelector('.catalog-content');
+                for (let i = 4; i < items.length; i++) {
+                    const itemHTML = `
+                <div class="catalog-content__card">
+                <img src="${items[i].img}" class="catalog-content__card-img">
+                <div class="catalog-content__card-descr">
+                    <div class="catalog-content__card-descr__name">
+                        ${items[i].name}
+                        </div>
+                        <div class="catalog-content__card-descr__price">
+                        ${items[i].price}
+                        </div>
+                    </div>
+                </div>`;
+                    catalog.innerHTML += itemHTML;
+                }
+                isShowedAllItems = true;
+            }
         }
     }
 });
+
+console.log(app);
